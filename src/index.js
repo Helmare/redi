ac7213e1732a7b65e1fbb89a5f1eaa8a7d8e3cb3
Hazdryx <hazdryx@gmail.com>
2021-11-08 10:56:13 -0600

    Created barebones redirect server.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
+/**
+ * @typedef {object} RediConfig
+ * @property {object} redirects
+ * @property {string} redirects.default
+ */
 require('dotenv').config();
 const express = require('express');
+const toml = require('toml');
+const fs = require('fs');
+const path = require('path');
+const debug = require('debug')('redi');
+
+// Load config
+debug('Loading config.toml...');
+const tomlData = fs.readFileSync(path.join(__dirname, '../config.toml'), 'utf8');
+/** @type {RediConfig} */
+const config = toml.parse(tomlData);
+debug('%O', config);
 
 // Setup app
 const app = express();
 app.use(require('helmet')());
 
-// Setup redirects.
+// Setup redirects
+Object.keys(config.redirects).forEach(path => {
+  if (path == "default") return; // Ignore default.
 
+  debug('Setting up redirect: %s => %s', path, config.redirects[path]);
+  app.all(path, (req, res) => {
+    res.redirect(config.redirects[path]);
+  });
+});
 
 // Fallback
-app.use((req, res) => {
-  res.redirect('https://hazdryx.me');
-});
+if (config.redirects.default) {
+  debug('Setting up fallback redirect to: %s', config.redirects.default);
+  app.use((req, res) => {
+    res.redirect(config.redirects.default);
+  });
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening to PORT: ${port}`);
+  debug('Listening to port %d...', port);
 });

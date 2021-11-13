dd9fea31b04bf105cee26ae63107014b72717049
Hazdryx <hazdryx@gmail.com>
2021-11-13 10:17:31 -0600

    Created an `upgrade` function for configurations.

--- src/config.js
+++ src/config.js
@@ -6,19 +6,41 @@ debug.enabled = true;
 
 /**
  * @typedef {object} RediConfig
- * @property {number} [version=1]
+ * @property {number} [version]
  * @property {object} redirects
  * @property {string} redirects.default
  */
+const CURRENT_VERSISON = 1;
 
-const DEFAULT_CONFIG = {
-  version: 1
+/**
+ * Upgrades a config file to the current version.
+ * @param {*} config
+ * @returns {RediConfig} 
+ */
+function upgrade(config) {
+  // Discover version.
+  if (!config.version) config.version = CURRENT_VERSISON;
+
+  // Upgrade.
+  if (config.version == CURRENT_VERSISON) {
+    return config;
+  }
+  else {
+    return null;
+  }
 }
 
 debug('Loading config.toml...');
 const tomlData = fs.readFileSync(path.join(__dirname, '../config.toml'), 'utf8');
-/** @type {RediConfig} */
-const config = { ...DEFAULT_CONFIG, ...toml.parse(tomlData) };
-debug('%O', config);
+const config = upgrade(toml.parse(tomlData));
+
+if (config) {
+  debug('%O', config);
+  module.exports = config;
+}
+else {
+  debug('Failed to load config from: %s', tomlData);
+  process.exit(1);
+}
 
 module.exports = config;
